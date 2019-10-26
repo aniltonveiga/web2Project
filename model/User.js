5b635d94a2c46404bc347c77db2fc0c3ec573a5a
Anilton Veiga <anilton.veigaa@gmail.com>
2019-10-26 15:06:38 -0300

    Added model schemas for mongoose

---
+++ model/User.js
@@ -0,0 +1,27 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+  user: {
+    type: String,
+    lowercase: true,
+    trim: true,
+    required: [true, "Digite um nome de usuário"],
+    unique: [true, "Já existe um usuário com esse nome"]
+  },
+  mail: {
+    type: String,
+    required: [true, "Digite um endereço de email"],
+    unique: [true, "Já existe um usuário com esse email"]
+  },
+  pass: {
+    type: String,
+    required: [true, "Cadastre uma senha"],
+    min: [4, "Digite pelo menos 4 caracteres"],
+    max: 12
+  },
+  friends: [{ type: Schema.Types.ObjectId, ref: "User" }]
+});
+
+module.exports = mongoose.model("User", UserSchema);
