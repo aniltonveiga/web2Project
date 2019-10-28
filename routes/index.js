0fd4809ef1a73566bb979102661386dc2ffadb15
Mateus Saggioro <mateus_saggioro@hotmail.com>
2019-10-28 13:53:36 -0300

    Visual changes and refactoring code

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function(req, res, next) {
-  res.render("index", { title: "Chat Legal!!", logado: req.session.key });
+  res.render("index", { title: "Chat", logado: req.session.key });
 });
 
 module.exports = router;
