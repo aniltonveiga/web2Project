23375825e026c8dae7523007e2ba304f201c3533
Anilton Veiga <anilton.veigaa@gmail.com>
2019-10-29 19:08:53 -0300

    change port

--- app.js
+++ app.js
@@ -67,4 +67,8 @@ app.use(function(err, req, res, next) {
   res.render("error");
 });
 
+app.listen(process.env.PORT || 3000, function() {
+  console.log(`Example app listening on port !`);
+});
+
 module.exports = app;
